f7b26d690a64d36c2a572b85be6a660325dbf4ac
tanksareforcowards <37027181+tanksareforcowards@users.noreply.github.com>
2020-04-17 23:10:35 -0400

    re-added Marianna's code that i overwrote like a moron
    Also cleaned up my own sloppy code, looks better now!

--- script.js
+++ script.js
@@ -1,40 +1,45 @@
 
-// API signup	723d7a87	live
 
 
 
+// bind to click
+// check switch text
+// switch search placeholder
+$("#Switch").on("click", function (e) {
+    e.preventDefault()
+    $(`[type="search"`).attr("placeholder", $("#Switch").text() == "Ingredients" ? "Search Ingredients" : "Search Recipes");
+    $("#Switch")
+    $("#Switch").text($("#Switch").text() == "Ingredients" ? "Recipes" : "Ingredients")
+}) 
+
+
+
+
+
+
+
+// ~~~ Recipe Facts Populator ~~~
+// This function will populate the recipe html with important details.  
 function populateRecipe (recipe) {
     document.getElementById("title").innerHTML = recipe.label;
     populateIngredients(recipe.ingredients)
     document.getElementById("calories").innerHTML = recipe.calories;
     document.getElementById("servings").innerHTML = recipe.yield;
     document.getElementById("recipe-img").src = recipe.image;
-    document.getElementById("diet-labels").innerHTML = recipe.dietLabels;
-    document.getElementById("health-labels").innerHTML = recipe.healthLabels;
+    document.getElementById("prep-time").innerHTML = recipe.time;
 }
 
+
+// ~~~ Ingredient List Populator ~~~
+// This function writes our ingredients into the webpage.
 function populateIngredients (ingredients) {
     var whatever = document.getElementById.ingredients;
     var ul = document.getElementById("ingredients-list");
-  
+    // This loop makes sure every ingredient will be listed.
     for (i = 0; i < ingredients.length; i++) {
-        // SHOULD BE a ul/li list in the html
-        // append text node to li
-        // append li to ul
         var li = document.createElement("LI");
         var ingredientDesc = document.createTextNode(ingredients[i].quantity + " " + ingredients[i].measure.label + " " + ingredients[i].food.label);
         li.appendChild(ingredientDesc);
         ul.appendChild(li);
     }
-
 }
-
-
-document.getElementById("title");
-
-
-
-
-// function takes the recipe info
-// uses that info to populate the recipe section of the webpage
-//
